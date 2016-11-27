1c6267a5c4162607acacabbb9a4b0ee22692d69f
hajigadung <hajigadung@gmail.com>
2016-11-27 23:58:08 +0700

    add fields of entity ref_kegiatan

--- application/models/entity/ref_kegiatan.php
+++ application/models/entity/ref_kegiatan.php
@@ -6,8 +6,7 @@ if (!defined("BASEPATH")) {
 
 class Ref_kegiatan extends LWS_model {
 
-    public function __construct() 
-	{
+    public function __construct() {
         parent::__construct("ref_kegiatan");
         $this->primary_key = "id_kegiatan";
     }
@@ -15,12 +14,23 @@ class Ref_kegiatan extends LWS_model {
     protected $attribute_labels = array(
         "id_kegiatan" => array("id_kegiatan", "ID Kegiatan"),
         "tahun" => array("tahun", "Tahun"),
+        "kegiatan_tugas_jabatan" => array("kegiatan_tugas_jabatan", "kegiatan_tugas_jabatan"),
+        "ak" => array("ak", "ak"),
+        "kuantitas" => array("kuantitas", "kuantitas"),
+        "kualitas" => array("kualitas", "kualitas"),
+        "waktu" => array("waktu", "waktu"),
+        "biaya" => array("biaya", "biaya"),
     );
     protected $rules = array(
         array("id_kegiatan", ""),
         array("tahun", ""),
-		);
-    
+        array("kegiatan_tugas_jabatan", ""),
+        array("ak", ""),
+        array("kuantitas", ""),
+        array("kualitas", ""),
+        array("waktu", ""),
+        array("biaya", ""),
+    );
     protected $related_tables = array();
     protected $attribute_types = array();
 
--- application/models/model_ref_kegiatan.php
+++ application/models/model_ref_kegiatan.php
@@ -9,6 +9,12 @@ class model_ref_kegiatan extends ref_kegiatan {
     protected $rules = array(
         array("id_kegiatan", "required|min_length[1]|max_length[30]"),
         array("tahun", "required|min_length[1]|max_length[30]"),
+        array("kegiatan_tugas_jabatan", ""),
+        array("ak", ""),
+        array("kuantitas", ""),
+        array("kualitas", ""),
+        array("waktu", ""),
+        array("biaya", ""),
     );
 
     public function __construct() {
@@ -19,19 +25,24 @@ class model_ref_kegiatan extends ref_kegiatan {
         return parent::get_all(array(
                     "id_kegiatan",
                     "tahun",
+                    "kegiatan_tugas_jabatan",
+                    "ak",
+                    "kuantitas",
+                    "kualitas",
+                    "waktu",
+                    "biaya",
                         ), FALSE, TRUE, FALSE, 1, TRUE, $force_limit, $force_offset);
     }
 
-    public function get_like($keyword=FALSE){
-        
+    public function get_like($keyword = FALSE) {
+
         $result = FALSE;
-        if($keyword){
+        if ($keyword) {
             $this->db->order_by("id_kegiatan", "asc");
-            $this->db->where(" lower(".$this->table_name.".tahun) LIKE lower('%".$keyword."%') OR lower(".$this->table_name.".id_kegiatan) LIKE lower('%".$keyword."%')", NULL, FALSE);
+            $this->db->where(" lower(" . $this->table_name . ".tahun) LIKE lower('%" . $keyword . "%') OR lower(" . $this->table_name . ".id_kegiatan) LIKE lower('%" . $keyword . "%')", NULL, FALSE);
             $result = $this->get_where();
         }
         return $result;
-        
     }
 
 }
