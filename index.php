73e12438158ab862cfbd44153e69a45dd65ec1cc
Fitrian Wahyu Ilahi <fitrianwahyuilahi45@gmail.com>
2016-11-30 18:10:24 +0700

    Update .Gitignore

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+define('ASSET_UPLOAD', dirname(__FILE__));
+
+/**
+ * set value TRUE, FALSE (boolean)
+ */
+define('ONCPANEL', FALSE);
+
+define('APPROOT', dirname(__FILE__) . DIRECTORY_SEPARATOR);
+define('APPPATH', APPROOT . "application" . DIRECTORY_SEPARATOR);
+require_once 'D:/xampp2/htdocs/lwscodeigniterwrapper/autoload.php';
+//require_once 'D:/xampp2/htdocs/lwscodeigniterwrapper/autoload.php';
+/* End of file index.php */
+/* Location: ./index.php */
+?>
